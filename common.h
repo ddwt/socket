0531696ff1120229775ce668e6e1e69c401d1da9
ddwt <33974770+ddwt@users.noreply.github.com>
2018-08-25 22:49:44 +0800

    v1.0
    公共头文件

---
+++ common.h
@@ -0,0 +1,36 @@
+/*************************************************************************
+	> File Name: common.h
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年06月10日 星期日 15时05分22秒
+ ************************************************************************/
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdarg.h>
+#include "head.h"
+
+#define MAX_SIZE 1024
+#define MASTER_PORT 8731
+#define _DEBUG
+
+
+
+#endif
