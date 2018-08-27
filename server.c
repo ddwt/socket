d4a09b14bcf82bbc0ca2cf17c4efa7cba3f9bdd1
ddwt <33974770+ddwt@users.noreply.github.com>
2018-08-27 23:25:11 +0800

    v2.0

--- server.c
+++ server.c
@@ -24,10 +24,15 @@
 #define PI9_HOST "192.168.1.48"
 #define PI10_HOST "192.168.1.49"
 
+#define link_warning_port 9999
+
 #define share_con_port 9000
 #define link_cpu_port 10000
 #define link_mem_port 10001
 #define link_disk_port 10002
+#define link_sys_port 10003
+#define link_user_port 10004
+#define link_proc_port 10005
 
 #define LOGADDR "/home/ddwt/文档/HZ/海贼项目/socket通信/Tcp项目/log/info.log"
 
@@ -36,9 +41,18 @@ int socket_connect(int, int, char *, char *);
 void socket_receive(char *, char *, char *);
 void get_log();
 char* get_time();
+void open_warning_port();
+void recv_file(char *str_file, int port, char *host, char *file_type);
 
 int main() {
-    
+    int PID;
+    PID = fork();
+    if (PID == 0) {
+	while (1) {
+	    open_warning_port();
+	}
+        return 0;  
+    } 
     char *buffer = (char *)calloc(1000, sizeof(char));
     while (1) {
         fflush(stdout);
@@ -108,7 +122,7 @@ int socket_connect(int sockfd, int port, char *host, char *file_type) {
     return sockfd;
 }
 
-void socket_receive(char *str_buff, char *buffer, char *client) {
+void socket_receive(char *str_file, char *buffer, char *client) {
     //1 socket()
     int sockfd;
     sockfd = socket_create();
@@ -119,8 +133,10 @@ void socket_receive(char *str_buff, char *buffer, char *client) {
     client_addr.sin_family = AF_INET;
     client_addr.sin_port = htons(port);
     client_addr.sin_addr.s_addr = inet_addr(host);
+    socklen_t len = sizeof(client_addr);
     if (connect(sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0) {
         DBG("connect error\n");
+        //printf("connect error\n");
         return ;
         //在记录中输出错误信息
     } else {
@@ -130,7 +146,6 @@ void socket_receive(char *str_buff, char *buffer, char *client) {
     //printf("链接成功\n");
     //sockfd = socket_connect(sockfd, port, host); 因为要获取链接地址　所以这里要把代码拆出来写
     //3 send()
-    
     int PID, count = 0;
     for (int i = 0; i < 6; i++) {
         count++;
@@ -139,113 +154,19 @@ void socket_receive(char *str_buff, char *buffer, char *client) {
     }
     if (PID > 0) {
         while (1) {
-            int a = 1;
-            scanf("%d", &a);
-            printf("父进程不能死!\n");
+            int con_sockfd = socket_connect(sockfd, share_con_port, HOSTIP, "log");
         }
     };
     if (count == 1) {
-        char buffer1[1000] = {0};
-        strcat(str_buff, "cpu.log");
-        strcpy(buffer1, "cpu");
-
-        int cpu_sockfd = socket_create();
-        struct sockaddr_in client_addr;
-        printf("开始链接专用端口\n");
-        cpu_sockfd = socket_connect(cpu_sockfd, link_cpu_port, host, "cpu");
-        if (cpu_sockfd > 0) {
-            printf("链接专用端口成功\n");
-        } else {
-            printf("链接专用端口失败\n");
-        }       
-        
-        int ans = send(cpu_sockfd, buffer1, strlen(buffer1), 0);
-        sleep(0.3);
-        memset(buffer1, 0, 1000);
-        fflush(stdout);  //接收前必须清除缓存
-        recv(cpu_sockfd, buffer1, 1000, 0);
-        DBG("receive = %s\n", buffer1);
-        if (strcmp(buffer1, "ok") == 0) {
-            sleep(3); //为啥睡３秒 等待client绑定端口开启监听
-            memset(buffer1, 0, 1000);
-            FILE *save_cpu = fopen(str_buff, "w"); //此处需要更具ＩＰ地址区分文件夹
-            fflush(stdout);
-            while (recv(cpu_sockfd, buffer1, 1000, 0)) {
-                fprintf(save_cpu, "%s", buffer1);
-                memset(buffer1, 0, 1000);
-            }
-            fflush(stdout);
-            fclose(save_cpu);
-            close(cpu_sockfd);
-            DBG("recv and save ok\n");
-        }
-        return ;
+        recv_file(str_file, link_cpu_port, host, "cpu");
     } else if (count == 2) {
-        char buffer2[1000] = {0};
-        strcat(str_buff, "mem.log");
-        strcpy(buffer2, "mem");
-        int mem_sockfd = socket_create();
-        mem_sockfd = socket_connect(mem_sockfd, link_mem_port, host, "mem");
-        if (mem_sockfd > 0) {
-            printf("链接专用端口成功\n");
-        } else {
-            printf("链接专用端口失败\n");
-        }              
-        send(mem_sockfd, buffer2, strlen(buffer2), 0);
-        memset(buffer2, 0, 1000);
-        fflush(stdout);
-        recv(mem_sockfd, buffer2, 1000, 0);
-        DBG("recvive = %s\n", buffer2);
-        if (strcmp(buffer2, "ok") == 0) {
-            sleep(0.3);
-            memset(buffer2, 0, 1000);
-
-            FILE *save_mem = fopen(str_buff, "w");
-            fflush(stdout);
-            while (recv(mem_sockfd, buffer2, 1000, 0)) {
-                fprintf(save_mem, "%s", buffer2);
-                memset(buffer2, 0, 1000);
-            }
-            fflush(stdout);
-            fclose(save_mem);
-            close(mem_sockfd);
-            DBG("recv and save ok\n");
-        }
-        return ;
+        recv_file(str_file, link_mem_port, host, "mem");
     } else if (count == 3) {
-        char buffer3[1000] = {0};
-        strcat(str_buff, "disk.log");
-        strcmp(buffer3, "disk");
-        int disk_sockfd = socket_create();
-        disk_sockfd = socket_connect(disk_sockfd, link_disk_port, host, "disk");
-        if (disk_sockfd > 0) {
-            printf("链接专用端口成功\n");
-        } else {
-            printf("链接专用端口失败\n");
-        }       
-        send(disk_sockfd, buffer3, strlen(buffer3), 0);
-        memset(buffer3, 0, 1000);
-        fflush(stdout);
-        recv(disk_sockfd, buffer3, 1000, 0);
-        DBG("receive = %s\n", buffer3);
-        if (strcmp(buffer3, "ok") == 0) {
-            sleep(0.3);
-            memset(buffer3, 0, 1000);
-
-            FILE *save_disk = fopen(str_buff, "w");
-            while (recv(disk_sockfd, buffer3, 1000, 0)) {
-                fprintf(save_disk, "%s", buffer3);
-            }
-            fflush(stdout);
-            fclose(save_disk);
-            close(disk_sockfd);
-            DBG("recv and save ok\n");
-        }
-        return ;
+        recv_file(str_file, link_disk_port, host, "disk");
     } else if (count == 4) {
-        return ;
+        recv_file(str_file, link_sys_port, host, "sys");
     } else if (count == 5) {
-        return ;
+        recv_file(str_file, link_user_port, host, "user");
     } else {
         return ;
     }   
@@ -271,4 +192,79 @@ char *get_time() {
     timenow = localtime(&now);
     char *ret = strdup(asctime(timenow));
     return ret;
+}
+
+void open_warning_port() {
+    //int warning_sockfd = socket_create();
+    //warning_sockfd = socket_connect(warning_sockfd, link_warning_port, );
+    int sockfd = socket_create();
+    struct sockaddr_in sock_addr;
+    sock_addr.sin_family = AF_INET;
+    sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    sock_addr.sin_port = htons(link_warning_port);
+    socklen_t len = sizeof(struct sockaddr_in);
+    if (bind(sockfd, (struct sockaddr *)&sock_addr, sizeof(sock_addr)) < 0) {
+        DBG("bind error!\n");
+        return ;
+        
+    }
+    if (listen(sockfd, 20) < 0) {
+        close(sockfd);
+        DBG("listen error\n");
+        return ;
+    }
+    int client_sockfd = accept(sockfd, (struct sockaddr *)&sock_addr, &len);
+    sleep(1);
+    char buffer[50] = {0};
+    recv(client_sockfd, buffer, 1000, 0);
+    FILE *f = fopen("/home/ddwt/文档/HZ/海贼项目/socket通信/Tcp项目/warning/warning.log", "a+");
+    fscanf(f, "%[^\n]s", buffer);
+    fclose(f);
+    return ;
+}
+
+void recv_file(char *str_file, int port, char *host, char *file) {
+    char buffer[1000] = {0};
+    char str_buff[51] = {0};
+    char file_type[51] = {0};
+    strcpy(file_type, file);
+    strcpy(str_buff, str_file);
+    strcat(file_type, ".log");
+    strcat(str_buff, file_type);
+    //printf("str_buff : %s\n", str_buff);
+    strcpy(buffer, file_type);
+    int sockfd = socket_create();
+    struct sockaddr_in client_addr;
+    //printf("开始链接专用端口\n");
+    sockfd = socket_connect(sockfd, port, host, file_type);
+    if (sockfd > 0) {
+        //printf("链接专用端口成功\n");
+    } else {
+        printf("链接专用端口失败\n");
+        return ;
+    }       
+    
+    send(sockfd, file, strlen(buffer), 0); 
+    sleep(0.3);
+    memset(buffer, 0, 1000);
+    fflush(stdout);  //接收前必须清除缓存
+    recv(sockfd, buffer, 1000, 0);
+    printf("recv : %s\n", buffer);
+    DBG("receive = %s\n", buffer);
+    if (strcmp(buffer, "ok") == 0) {
+        sleep(3); //为啥睡３秒 等待client绑定端口开启监听
+        memset(buffer, 0, 1000);
+        FILE *save_f = fopen(str_buff, "w"); //此处需要更具ＩＰ地址区分文件夹
+        if (save_f == NULL) printf("%s : 文件打开失败", str_buff);
+        fflush(stdout);
+        while (recv(sockfd, buffer, 1000, 0)) {
+            fprintf(save_f, "%s", buffer);
+            memset(buffer, 0, 1000);
+        }
+        fflush(stdout);
+        fclose(save_f);
+        close(sockfd);
+        DBG("recv and save ok\n");
+    }
+    return ;   
 }
